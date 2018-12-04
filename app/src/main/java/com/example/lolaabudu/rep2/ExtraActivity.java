64ca0c5d3f731e0fdd01a61565f6f729a1a8d93a
rosannaNaomy <naomypursuit.org>
2018-12-03 23:01:36 -0500

    naomy added extra xml

--- app/src/main/java/com/example/lolaabudu/rep2/ExtraActivity.java
+++ app/src/main/java/com/example/lolaabudu/rep2/ExtraActivity.java
@@ -1,5 +1,12 @@
 package com.example.lolaabudu.rep2;
 
+import android.os.Bundle;
+
 public class ExtraActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Extra);
+    }
 }
